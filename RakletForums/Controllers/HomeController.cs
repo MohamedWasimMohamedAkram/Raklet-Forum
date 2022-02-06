bac6fbf8f79e36dbe345f3b188f3e00114c1b8f2
Mohamed Wasim Mohamed Akram <wasim.mohamed@ug.bilkent.edu.tr>
2022-02-06 22:36:54 +0300

    Made Home Changes

--- RakletForums/Controllers/HomeController.cs
+++ RakletForums/Controllers/HomeController.cs
@@ -4,15 +4,63 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RakletForums.Data;
+using RakletForums.Data.Models;
 using RakletForums.Models;
+using RakletForums.Models.Forum;
+using RakletForums.Models.Home;
+using RakletForums.Models.Post;
 
 namespace RakletForums.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly IPost _postService;
+
+        public HomeController(IPost postService)
+        {
+            _postService = postService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = BuildHomeIndexModel();
+            return View(model);
+        }
+
+        private HomeIndexModel BuildHomeIndexModel()
+        {
+            var latestPosts = _postService.GetLatestPosts(10);
+
+            var posts = latestPosts.Select(post => new PostListingModel
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorName = post.User.UserName,
+                AuthorId = post.User.Id,
+                AuthorRating = post.User.Rating,
+                DatePosted = post.Created.ToString(),
+                RepliesCount = post.Replies.Count(),
+                Forum = GetForumListingForPost(post)
+            });
+
+            return new HomeIndexModel
+            {
+                LatestPosts = posts,
+                SearchQuery = ""
+            };
+        }
+
+        private ForumListingModel GetForumListingForPost(Post post)
+        {
+            var forum = post.Forum;
+            return new ForumListingModel
+            {
+                Id = forum.Id,
+                Name = forum.Title,
+                ImageUrl = forum.ImageUrl
+            };
         }
 
         public IActionResult About()
