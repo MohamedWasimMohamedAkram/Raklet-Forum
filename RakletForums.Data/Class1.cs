dd5d6c0a8b644bb4f130eb5635d8ef9874c30071
Mohamed Wasim Mohamed Akram <wasim.mohamed@ug.bilkent.edu.tr>
2022-02-06 15:05:29 +0300

    Services

--- RakletForums.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RakletForums.Data
-{
-    public class Class1
-    {
-    }
-}
--- RakletForums/Controllers/ManageController.cs
+++ RakletForums/Controllers/ManageController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RakletForums.Data.Models;
 using RakletForums.Models;
 using RakletForums.Models.ManageViewModels;
 using RakletForums.Services;
