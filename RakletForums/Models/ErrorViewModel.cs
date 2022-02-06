a9b23df76af3f97519669440e82a72d8f7c74090
Mohamed Wasim Mohamed Akram <wasim.mohamed@ug.bilkent.edu.tr>
2022-02-06 13:43:14 +0300

    Add project files.

---
+++ RakletForums/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RakletForums.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
